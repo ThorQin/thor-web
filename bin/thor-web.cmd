52d3fcac405ee6e6a89e02e6e1a23e80d4d25d2c
Thor Qin <thor.qin@outlook.com>
2020-05-13 01:03:09 +0800

    Upgrade to 1.1.2

--- bin/thor-web.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\thor-web\bin\thor-web-init.cjs" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\thor-web\bin\thor-web-init.cjs" %*
-)
